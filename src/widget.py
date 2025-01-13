5be8724b2ff7b70afe76ed8fadb259acc9f3c2fb
zyablova svetlana <hisamutdinovramil15@gmail.com>
2025-01-14 00:38:51 +1100

    init

--- src/widget.py
+++ src/widget.py
@@ -1,9 +1,11 @@
-from src.masks import get_mask_card_number as mask_card_number, get_mask_account_number as mask_account_number
+# from src.masks import get_mask_card_number as mask_card_number,
+# get_mask_account_number as mask_account_number
 
 def mask_account_card(account_number):
     parts = account_number.split()
     if len(parts) > 1:
-        # Если номер карты состоит из нескольких частей (например, Visa Gold 5999 4142 2842 6353)
+        # Если номер карты состоит из нескольких частей
+        # (например, Visa Gold 5999 4142 2842 6353)
         masked_parts = []
         for part in parts[1:]:
             if len(part) <= 4:
@@ -13,13 +15,16 @@ def mask_account_card(account_number):
             masked_parts.append(masked_part)
         return f"{parts[0]} {' '.join(masked_parts)}"
     else:
-        # Если номер карты представлен одной строкой (например, MasterCard 1234567890123456)
+        # Если номер карты представлен одной строкой
+        # (например, MasterCard 1234567890123456)
         return f"{parts[0]} {'*' * (len(parts[0]) - 4)}{parts[0][-4:]}"
 
-def mask_card_number(card_number: str) -> str:
+
+def _mask_card_number(card_number: str) -> str:
     return card_number[:6] + '*' * (len(card_number) - 10) + card_number[-4:]
 
-def mask_account_number(account_number: str) -> str:
+
+def _mask_account_number(account_number: str) -> str:
     return '*' * (len(account_number) - 4) + account_number[-4:]
 
 
