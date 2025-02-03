ae3674e6361a29c0857a56f2032297138251b1dc
zyablova svetlana <hisamutdinovramil15@gmail.com>
2025-02-03 22:50:17 +1100

    init

--- src/generators.py
+++ src/generators.py
@@ -1,15 +1,12 @@
 def filter_by_currency(transactions, currency_code):
-    """Фильтрует транзакции по заданной валюте и возвращает итератор."""
     for transaction in transactions:
-        if transaction.get('operationAmount', {}).get('currency', {}).get('code') == currency_code:
+        if transaction["operationAmount"]["currency"]["code"] == currency_code:
             yield transaction
 
 def transaction_descriptions(transactions):
-    """Генератор, который возвращает описание каждой операции."""
     for transaction in transactions:
-        yield transaction.get('description')
+        yield transaction["description"]
 
 def card_number_generator(start, stop):
-    """Генератор, который выдает номера банковских карт в заданном диапазоне."""
-    for number in range(start, stop + 1):
-        yield f"{number:016d}"  # Форматирование номера карты
+    for i in range(start, stop + 1):
+        yield f'{i:016}'.replace(' ', '0').replace('0', 'X')[::-1].replace('X', ' ').strip()[::-1]
